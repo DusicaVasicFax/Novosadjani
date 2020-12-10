dfa233b1f6f0696622a9128acf9947001616caa8
Dusica Vasic <d.vasic98@gmail.com>
2020-12-10 21:57:27 +0100

    Enemy right path..

--- Enemy/Enemy.py
+++ Enemy/Enemy.py
@@ -5,4 +5,4 @@ from PyQt5.QtWidgets import QLabel, QVBoxLayout, QGraphicsPixmapItem
 class Enemy(QGraphicsPixmapItem):
     def __init__(self, parent=None):
         QGraphicsPixmapItem.__init__(self, parent)
-        self.setPixmap(QPixmap("../images/alien_1/alien_1_cycle_1_55x55.png"))
+        self.setPixmap(QPixmap("images/alien_1/alien_1_cycle_1_55x55.png"))
