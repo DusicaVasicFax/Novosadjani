a13d459e460a8ae984b22d3f57b5db4447622205
Dusica Vasic <d.vasic98@gmail.com>
2020-12-10 20:37:52 +0100

    enemy setup vol2

--- Enemy/Enemy.py
+++ Enemy/Enemy.py
@@ -1,7 +1,15 @@
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QGraphicsPixmapItem
 
-class Enemy():
+
+class Enemy(QGraphicsPixmapItem):
     def __init__(self):
         super().__init__()
+
+    def draw(self):
+        self.setPixmap(QPixmap("../images/alien_1/alien_1_cycle_1_11x11.png"))
+
+    def move(self, vel):
+        self.y += vel
+
 
--- Enemy/EnemyGraphics.py
+++ Enemy/EnemyGraphics.py
@@ -1,12 +1,22 @@
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QGraphicsPixmapItem
 
 
-class EnemyGraphics:
-    def __init__(self, x: float =0, y:float =0, img: str = ''):
+class EnemyGraphics(QGraphicsPixmapItem):
+
+    def __init__(self):
         super().__init__()
+        self.setPixmap(QPixmap("../images/alien_1/alien_1_cycle_1_11x11.png"))
 
-        self.x = x
-        self.y = y
-        self.img = img
+    def imgShow(self, x: float, y: float):
+        labelImg = QLabel(self)
+        img = QPixmap("../images/alien_1/alien_1_cycle_1_11x11.png")
+        labelImg.setPixmap(img)
+        vbox = QVBoxLayout()
+        vbox.addWidget(labelImg)
+        self.setLayout(vbox)
+        self.Show()
 
+    #def moveEnemy(self, move_x: float, move_y: float):
+        #self.x = self.x + move_x
+        #self.y = self.y + move_y
