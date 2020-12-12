bf4433afacb361687a40b46d63a32944613399c7
Dusan Mazic <dusanmazic123@gmail.com>
2020-12-12 18:38:06 +0100

    Tryed to add bullets to a player but it doesnt work

---
+++ Bullets.py
@@ -0,0 +1,41 @@
+from PyQt5.QtCore import Qt, QBasicTimer
+
+from PyQt5.QtGui import QPixmap, QBrush
+
+from PyQt5.QtWidgets import QGraphicsPixmapItem, QGraphicsScene, QGraphicsRectItem, QGraphicsView, QApplication
+
+
+
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 900
+BULLET_SPEED = 10  # pix/frame
+BULLET_FRAMES = 50
+FRAME_TIME_MS = 16  # ms/frame
+
+
+class Player(QGraphicsPixmapItem):
+    def __init__(self, parent=None):
+        QGraphicsPixmapItem.__init__(self, parent)
+        self.setPixmap(QPixmap("images/ship/ship_55x55.png"))
+
+class Bullet(QGraphicsPixmapItem):
+    def __init__(self, offset_x, offset_y, parent = None):
+        QGraphicsPixmapItem.__init__(self, parent)
+        self.setPixmap(QPixmap("bullet_11x11.png"))
+        self.offset_x = offset_x
+        self.offset_y = offset_y
+        self.active = False
+        self.frames = 0
+
+    def game_update(self, keys_pressed, player):
+        if not self.active:
+            if Qt.Key_Space in keys_pressed:
+                self.active = True
+                self.setPos(player.x()+self.offset_x,player.y()+self.offset_y)
+                self.frames = BULLET_FRAMES
+        else:
+            self.setPos(self.x(),self.y()-BULLET_SPEED)
+            self.frames -= 1
+            if self.frames <= 0:
+                self.active = False
+                self.setPos(SCREEN_WIDTH, SCREEN_HEIGHT)
--- Game.py
+++ Game.py
@@ -1,10 +1,13 @@
 from PyQt5.QtCore import Qt, QBasicTimer
 from PyQt5.QtGui import QBrush
-from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsRectItem
 from Player import Player
+from Bullets import Bullet
 
 SCREEN_WIDTH = 1400
 SCREEN_HEIGHT = 900
+PLAYER_BULLET_X_OFFSETS = [0.45]
+PLAYER_BULLET_Y = 15
 FRAME_TIME_MS = 16  # TODO change this later
 
 
@@ -16,14 +19,25 @@ class Game(QGraphicsScene):
         self.timer = QBasicTimer()
         self.timer.start(FRAME_TIME_MS, self)
 
+        bg = QGraphicsRectItem()
+        bg.setRect(-1, -1, SCREEN_WIDTH + 2, SCREEN_HEIGHT + 2)
+        bg.setBrush(QBrush(Qt.blue))
+        self.addItem(bg)
+
         # ADDING THE PLAYER (Current support is for only one player)
         self.player = Player()
         self.player.setPos((SCREEN_WIDTH - self.player.pixmap().width()) / 2,
                            (SCREEN_HEIGHT - self.player.pixmap().height()) / 2)
 
+        self.bullets = [Bullet(PLAYER_BULLET_X_OFFSETS[0], PLAYER_BULLET_Y),
+                        Bullet(PLAYER_BULLET_X_OFFSETS[1], PLAYER_BULLET_Y)]
+
+        for b in self.bullets:
+            b.setPos(SCREEN_WIDTH, SCREEN_HEIGHT)
+            self.addItem(b)
+
         self.addItem(self.player)
 
-        # Showing the view
         self.view = QGraphicsView(self)
         self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
@@ -44,3 +58,5 @@ class Game(QGraphicsScene):
 
     def game_update(self):
         self.player.game_update(self.keys_pressed)
+        for b in self.bullets:
+            b.game_update(self.keys_pressed, self.player)
