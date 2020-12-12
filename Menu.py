333f935f1043d7f617dad17d4e31ff292af6cf6b
Kristian Tot <k.tot@vegaitsourcing.rs>
2020-12-12 13:59:37 +0100

    Added game.py and refactored the code a little bit. Now we support player movement on the screen

--- Menu.py
+++ Menu.py
@@ -1,16 +1,9 @@
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, \
-    QLabel, QGraphicsScene, QGraphicsView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QPushButton, \
+    QLabel
 from PyQt5.QtCore import Qt
-from Player import Player
 import sys
-
-from PyQt5.uic.properties import QtWidgets, QtCore, QtGui
-
-SCREEN_WIDTH = 1400
-SCREEN_HEIGHT = 900
-
-
+from Game import Game
 
 
 class Menu(QMainWindow):
@@ -30,7 +23,6 @@ class Menu(QMainWindow):
         self.move(qr.topLeft())
 
     def ui_components(self):
-
         head = QLabel("Space invaders", self)
         head.setGeometry(550, 300, 300, 60)
 
@@ -46,29 +38,12 @@ class Menu(QMainWindow):
 
         self.new_game = QPushButton("New game", self)
         self.new_game.setStyleSheet('QPushButton {background-color: #ffffff; color: black; border: none}')
-
         self.new_game.setGeometry(650, 360, 100, 50)
-
         self.new_game.clicked.connect(self.on_new_game_clicked)
 
     def on_new_game_clicked(self):
-        # print("RADIIII")
-        view = Game()
+        Game()
         self.hide()
-
-class Game(QGraphicsScene):
-    def __init__(self, parent=None):
-        QGraphicsScene.__init__(self, parent=parent)
-        self.player = Player()
-        self.player.setPos((SCREEN_WIDTH - self.player.pixmap().width()) / 2,
-                           (SCREEN_HEIGHT - self.player.pixmap().height()) / 2)
-
-        self.addItem(self.player)
-        self.view = QGraphicsView(self)
-        self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.view.setFixedSize(SCREEN_WIDTH, SCREEN_HEIGHT)
-        self.view.show()
 
 
 def start():
--- Player.py
+++ Player.py
@@ -1,31 +1,28 @@
-
 from PyQt5.QtCore import Qt
 
 from PyQt5.QtGui import QPixmap
 
 from PyQt5.QtWidgets import QGraphicsPixmapItem
 
-PLAYER_SPEED = 3
+PLAYER_SPEED = 10
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 900
 
 
 class Player(QGraphicsPixmapItem):
     def __init__(self, parent=None):
         QGraphicsPixmapItem.__init__(self, parent)
-        self.setPixmap(QPixmap("images/alien_1/alien_1_cycle_1_55x55.png"))
+        self.setPixmap(QPixmap("images/ship/ship_55x55.png"))
 
-    # def game_update(self, keys_pressed):
-    #     dx = 0
-    #     dy = 0
-    #     if Qt.Key_Left in keys_pressed:
-    #         dx -= PLAYER_SPEED
-    #     if Qt.Key_Right in keys_pressed:
-    #         dx += PLAYER_SPEED
-    #     if Qt.Key_Up in keys_pressed:
-    #         dy -= PLAYER_SPEED
-    #         print(self.y() - dy)
-    #     if Qt.Key_Down in keys_pressed:
-    #         dy += PLAYER_SPEED
-    #     self.setPos(self.x()+dx, self.y()+dy)
-
-
-
+    def game_update(self, keys_pressed):
+        dx = 0
+        dy = 0
+        if Qt.Key_Left in keys_pressed and self.x() - PLAYER_SPEED > 0:
+            dx -= PLAYER_SPEED
+        if Qt.Key_Right in keys_pressed and self.x() + PLAYER_SPEED < SCREEN_WIDTH - self.pixmap().width():
+            dx += PLAYER_SPEED
+        if Qt.Key_Up in keys_pressed and self.y() - PLAYER_SPEED > 0:
+            dy -= PLAYER_SPEED
+        if Qt.Key_Down in keys_pressed and self.y() + PLAYER_SPEED < SCREEN_HEIGHT - self.pixmap().height():
+            dy += PLAYER_SPEED
+        self.setPos(self.x() + dx, self.y() + dy)
