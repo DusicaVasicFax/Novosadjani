45e30399e50590aaadeb8ba479009454bf112b63
Kristian Tot <k.tot@vegaitsourcing.rs>
2021-01-03 20:42:04 +0100

    Fixed conflict and showed score

--- Score/Score.py
+++ Score/Score.py
@@ -1,13 +1,14 @@
 from PyQt5.QtWidgets import QLabel
+from Constants import *
 
 
 class Score(QLabel):
-    def __init__(self, parent=None):
+    def __init__(self, parent):
         QLabel.__init__(self, parent)
         self.score = 0
-        prozor = QLabel("Score: ")
-        prozor.setGeometry(430, 10, 500, 50)
-        prozor.setStyleSheet("color: orange")
+        self.setText("Score: 0")
+        self.setGeometry(SCREEN_WIDTH - 100, 10, 100, 20)
+        self.setStyleSheet("color: orange")
 
     def print_results(self):
         self.score += 1
