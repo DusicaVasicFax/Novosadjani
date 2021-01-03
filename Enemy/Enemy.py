7efa7f3cb185873b6c9e7075f75c568aff6a67c0
Kristian Tot <k.tot@vegaitsourcing.rs>
2021-01-03 10:25:35 +0100

    refactored to qlabel and deleted wrong images

--- Enemy/Enemy.py
+++ Enemy/Enemy.py
@@ -1,28 +1,31 @@
+from PyQt5.QtCore import QRect
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QGraphicsPixmapItem
+from PyQt5.QtWidgets import QGraphicsPixmapItem, QLabel
 from Constants import *
 
 
-class Enemy(QGraphicsPixmapItem):
-    def __init__(self, x, y, alien_type, parent=None):
-        QGraphicsPixmapItem.__init__(self, parent)
+class Enemy(QLabel):
+    def __init__(self, i, j, parent):
+        QLabel.__init__(self, parent)
 
-        if alien_type == '1':
-            self.setPixmap(QPixmap("images/alien_1/alien_1_cycle_1_55x55.png"))
-        elif alien_type == '2':
-            self.setPixmap(QPixmap("images/alien_2/alien_2_cycle_1_55x55.png"))
+        if j == 0:
+            self.setPixmap(QPixmap("images/aliens/alien_1.png"))
+        elif 0 < j <= 2:
+            self.setPixmap(QPixmap("images/aliens/alien_2.png"))
         else:
-            self.setPixmap(QPixmap("images/alien_3/alien_3_cycle_1_55x55.png"))
-        self.setPos(self.calculate_start_position_x(x), self.calculate_start_position_y(y))
+            self.setPixmap(QPixmap("images/aliens/alien_3.png"))
+        self.setGeometry(self.calculate_start_position_x(i), self.calculate_start_position_y(j), self.pixmap().width(),
+                         self.pixmap().height())
+        self.setStyleSheet("border: 1px solid white;")
         self.moves = 0
         self.direction = 1
         self.health = 1
 
     def calculate_start_position_x(self, i) -> float:
-        return (SCREEN_WIDTH - self.pixmap().width()) / 5 + i * 90
+        return (SCREEN_WIDTH - self.width()) / 5 + i * 90
 
     def calculate_start_position_y(self, j) -> float:
-        return ((j + 1) * self.pixmap().height()) + 20 * j
+        return ((j + 1) * self.height()) + 30 * j
 
     def game_update(self):
         if self.moves == 25:
@@ -42,15 +45,15 @@ class Enemy(QGraphicsPixmapItem):
             self.move_left()
 
     def move_right(self) -> None:
-        self.setX(self.x() + PLAYER_SPEED)
+        self.setGeometry(self.x() + PLAYER_SPEED, self.y(), self.width(), self.height())
         self.inc_moves()
 
     def move_left(self) -> None:
-        self.setX(self.x() - PLAYER_SPEED)
+        self.setGeometry(self.x() - PLAYER_SPEED, self.y(), self.width(), self.height())
         self.inc_moves()
 
     def move_down(self) -> None:
-        self.setY(self.y() + self.pixmap().height())
+        self.setGeometry(self.x(), self.y() + self.height(), self.width(), self.height())
 
     def change_direction(self, down=False) -> None:
         if down:
@@ -62,13 +65,15 @@ class Enemy(QGraphicsPixmapItem):
         self.moves += 1
 
     def check_if_enemy_is_hit(self, bullet) -> bool:
-        x_coordinate_in_range = (self.x() <= bullet.x() <= self.x() + self.pixmap().width() - 35) or \
-                                (self.x() <= bullet.x() + 20 <= self.x() + self.pixmap().width() - 35)
-        y_coordinate_in_range = (self.y() <= bullet.y() <= self.y() + self.pixmap().height() - 35) or \
-                                (self.y() <= bullet.y() + bullet.pixmap().height() <=
-                                 self.y() + self.pixmap().height() - 35)
+        x = self.x()
+        x1 = self.x() + self.width()
+        x_coordinate_in_range = (x <= bullet.x() <= x1) or \
+                                (x <= bullet.x() + bullet.width() <= x1)
+        y = self.y()
+        y1 = self.y() + self.height()
+        y_coordinate_in_range = (y <= bullet.y() <= y1) or \
+                                (y <= bullet.y() + bullet.height() <= y1)
 
-        # TODO figure out the magic number for the bullet.y() since i guess bullet.pixmap().height is not accurate
         hit = x_coordinate_in_range and y_coordinate_in_range
 
         if hit:
@@ -76,5 +81,3 @@ class Enemy(QGraphicsPixmapItem):
             self.health -= 1
 
         return self.health == 0
-
-
--- Shield/Shield.py
+++ Shield/Shield.py
@@ -1,32 +1,34 @@
-from PyQt5.QtGui import QPixmap, QPen, QColor
-from PyQt5.QtWidgets import QGraphicsPixmapItem
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QLabel
 
 from Constants import *
 
 
-class Shield(QGraphicsPixmapItem):
-    def __init__(self, i, parent=None):
-        QGraphicsPixmapItem.__init__(self, parent)
-        self.setPixmap(QPixmap("images/shield/shield_108x132_cut.png"))
-        self.setPos(self.calculate_start_position_x(i), self.calculate_start_position_y())
+class Shield(QLabel):
+    def __init__(self, i, parent):
+        QLabel.__init__(self, parent)
+        self.setPixmap(QPixmap("images/shield/test.png"))
+        self.setGeometry(self.calculate_start_position_x(i), self.calculate_start_position_y(), self.pixmap().width(),
+                         self.pixmap().height())
+        self.setStyleSheet("border: 1px solid white;")
         self.active = True,
         self.health = 6
 
-    def calculate_start_position_x(self, x) -> float:
+    def calculate_start_position_x(self, x) -> int:
         return SCREEN_WIDTH - 100 - (x * 535) - self.pixmap().width()
 
-    def calculate_start_position_y(self) -> float:
+    def calculate_start_position_y(self) -> int:
         return SCREEN_HEIGHT - self.pixmap().height() - 70
 
     def check_if_shield_is_destroyed(self) -> bool:
         return self.health <= 0
 
     def check_if_shield_is_hit(self, bullet) -> None:
-        x_coordinate_in_range = (self.x() <= bullet.x() <= self.x() + self.pixmap().width() - 35) or \
-                                (self.x() <= bullet.x() + 20 <= self.x() + self.pixmap().width() - 35)
-        y_coordinate_in_range = (self.y() <= bullet.y() <= self.y() + self.pixmap().height() - 35) or \
-                                (self.y() <= bullet.y() + bullet.pixmap().height() <=
-                                 self.y() + self.pixmap().height() - 35)
+        x_coordinate_in_range = (self.x() <= bullet.x() <= self.x() + self.width()) or \
+                                (self.x() <= bullet.x() + bullet.width() <= self.x() + self.width())
+        y_coordinate_in_range = (self.y() <= bullet.y() <= self.y() + self.height()) or \
+                                (self.y() <= bullet.y() + bullet.height() <=
+                                 self.y() + self.height())
 
         # TODO figure out the magic number for the bullet.y() since i guess bullet.pixmap().height is not accurate
         hit = x_coordinate_in_range and y_coordinate_in_range
@@ -38,4 +40,5 @@ class Shield(QGraphicsPixmapItem):
                 self.setPixmap(QPixmap("images/shield/shield_108x132_cut_damage_left.png"))
             elif self.health == 2:
                 self.setPixmap(QPixmap("images/shield/shield_108x132_cut_damage_both.png"))
-
+            elif self.health <= 0:
+                self.close()
