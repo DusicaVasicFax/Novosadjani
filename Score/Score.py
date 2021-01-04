154a60b3a62b3a8fada34d2998ea1d931771dac8
Dusan Mazic <dusanmazic123@gmail.com>
2021-01-04 13:15:02 +0100

    Fixed the score

--- Score/Score.py
+++ Score/Score.py
@@ -1,3 +1,4 @@
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QLabel
 from Constants import *
 
@@ -7,7 +8,8 @@ class Score(QLabel):
         QLabel.__init__(self, parent)
         self.score = 0
         self.setText("Score: 0")
-        self.setGeometry(SCREEN_WIDTH - 100, 10, 100, 20)
+        self.setGeometry(SCREEN_WIDTH - 100, 5, 100, 20)
+        self.setFont(QFont('Times', 12))
         self.setStyleSheet("color: orange")
 
     def print_results(self):
